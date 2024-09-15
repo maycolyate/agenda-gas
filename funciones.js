99996e454897038ad779e81c8f63cc96a0f66560
maycolyate <maycolyae15@gmail.com>
2024-09-15 18:42:42 -0500

    video 9 obtener documento y generar tabla

--- funciones.js
+++ funciones.js
@@ -6,3 +6,9 @@ function doGet() {
 function obtenerDatosHtml(nombre) {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos() {
+    let hoja = SpreadsheetApp.openById('1ACLYbDAf_aVZbyJYQjFA_TxN1MA9os8DvLC5hcYJqFE').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
